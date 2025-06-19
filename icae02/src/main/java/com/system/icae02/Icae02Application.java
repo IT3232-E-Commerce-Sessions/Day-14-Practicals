6b0288b02f2b7ea4881e1a0955cdc44f06f334f2
Udara Nalawansa <udarakavishka13@gmail.com>
2025-06-20 00:24:48 +0530

    feat:Add initial project structure with Maven configuration and Spring Boot setup

---
+++ icae02/src/main/java/com/system/icae02/Icae02Application.java
@@ -0,0 +1,13 @@
+package com.system.icae02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Icae02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Icae02Application.class, args);
+	}
+
+}
---
+++ icae02/src/test/java/com/system/icae02/Icae02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.icae02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Icae02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
